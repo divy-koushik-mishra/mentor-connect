e5ff01175f271994eacad3098e8a4b6353a9d847
divy-koushik <dk@adizen.ai>
2024-10-26 22:21:19 +0530

    gtm

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 const TopBar = () => {
   return (
-    <div className="bg-[#101010] py-1 text-white text-center top-0 sticky">An initiative by IIT alumuni</div>
+    <div className="bg-[#101010] py-1 z-50 text-white text-center top-0 sticky">An initiative by IIT alumuni</div>
   )
 }
 
